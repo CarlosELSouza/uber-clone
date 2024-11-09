3717a4d8c584b53dc6db2e59ca8db0d922c1fe93
Carlos Eduardo Louro Souza <carloselsouza@Laptop-de-Carlos.local>
2024-11-09 15:36:08 -0300

    Assets

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Rides = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text className="text-zinc-500">Home</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Rides;
